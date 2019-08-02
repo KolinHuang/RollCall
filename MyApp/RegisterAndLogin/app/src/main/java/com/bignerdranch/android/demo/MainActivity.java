90212eba7e87a9a114f472e77fad1d4e2c25d7fb
黄玉才 <huangyucai@huangyucaideMacBook-Pro.local>
2019-08-02 14:52:50 +0800

    使用SQLite实现的注册和登录页面

---
+++ MyApp/RegisterAndLogin/app/src/main/java/com/bignerdranch/android/demo/MainActivity.java
@@ -0,0 +1,140 @@
+package com.bignerdranch.android.demo;
+
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
+//import butterknife.OnClick;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        /**
+//         * 一定 一定 一定记得加这句，而且是固定位置，在setContentView()之下
+//         * 否则无论写的什么逻辑  都不会在Activity中起作用
+//         */
+//        ButterKnife.bind(this);
+//    }
+//
+//    @BindView(R.id.bt_main_logout)
+//    Button mBtMainLogout;
+//
+//    @OnClick({
+//            R.id.bt_main_logout
+//    })
+//    public void onClick(View view){
+//        switch (view.getId()) {
+//            case R.id.bt_main_logout:
+//                Intent intent = new Intent(this, loginActivity.class);
+//                startActivity(intent);
+//                finish();
+//                break;
+//        }
+//    }
+//}
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private RadioGroup mTabRadioGroup;
+
+    private List<Fragment> mFragments;
+    private FragmentPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        // find view
+        mViewPager = findViewById(R.id.fragment_vp);
+        mTabRadioGroup = findViewById(R.id.tabs_rg);
+        // init fragment
+
+        // init view pager
+        mAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), mFragments);
+        mViewPager.setAdapter(mAdapter);
+        // register listener
+        mViewPager.addOnPageChangeListener(mPageChangeListener);
+        mTabRadioGroup.setOnCheckedChangeListener(mOnCheckedChangeListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mViewPager.removeOnPageChangeListener(mPageChangeListener);
+    }
+
+    private ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            RadioButton radioButton = (RadioButton) mTabRadioGroup.getChildAt(position);
+            radioButton.setChecked(true);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener mOnCheckedChangeListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+//            for (int i = 0; i < group.getChildCount(); i++) {
+//                if (group.getChildAt(i).getId() == checkedId) {
+//                    mViewPager.setCurrentItem(i);
+//                    return;
+//                }
+//            }
+        }
+    };
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> mList;
+
+        public MyFragmentPagerAdapter(FragmentManager fm, List<Fragment> list) {
+            super(fm);
+            this.mList = list;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return this.mList == null ? null : this.mList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return this.mList == null ? 0 : this.mList.size();
+        }
+    }
+
+}
+
